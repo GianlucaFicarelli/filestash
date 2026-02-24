2836d6769594b84e90d0666d48e88f594aec1c5f
Gianluca Ficarelli <gianluca.ficarelli@gmail.com>
2026-02-25 00:21:33 +0100

    fix: format after merge

--- server/plugin/plg_search_sqlitefts/index.go
+++ server/plugin/plg_search_sqlitefts/index.go
@@ -2,9 +2,9 @@ package plg_search_sqlitefts
 
 import (
 	. "github.com/mickael-kerjean/filestash/server/common"
+	. "github.com/mickael-kerjean/filestash/server/plugin/plg_search_sqlitefts/config"
 	. "github.com/mickael-kerjean/filestash/server/plugin/plg_search_sqlitefts/crawler"
 	. "github.com/mickael-kerjean/filestash/server/plugin/plg_search_sqlitefts/workflow"
-	. "github.com/mickael-kerjean/filestash/server/plugin/plg_search_sqlitefts/config"
 )
 
 func init() {
